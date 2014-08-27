69000e7d52f8c580bbece3ba1510f85987747f9f
Eric Dill <edill@bnl.gov>
2014-08-27 12:29:06 -0400

    ENH: Working code to seed data broker with spec data

--- miniature-hipster/pyspec_into_broker.py
+++ miniature-hipster/pyspec_into_broker.py
@@ -5,7 +5,7 @@ from pyspec.spec import SpecDataFile, FileProcessor
 from matplotlib import pyplot
 import numpy as np
 import sys
-from metadataStore.userapi.commands import create, record
+# from metadataStore.userapi.commands import create, log
 
 spec_folder_path = "c:\\DATA\\X1A2\\X1Data\\"
 spec_folder_path = "/home/edill/X1Data/"
@@ -40,17 +40,10 @@ wavelength = []
 ub = []
 img_files=[]
 for scan_no in scan_nos:
-    create(header={'scan_id': scan_no})
-    create(beamline_config={'scan_id': scan_no})
-    create(event_descriptor={'scan_id': scan_no,
-                             'descriptor_name': 'hkl_scan',
-                             'event_type_id': 1,
-                             'tag': 'experimental'})
-    temp = sf[scan_no].Tsam
-    motors = sf[scan_no].getSIXCAngles()
-    wavelength = sf[scan_no].wavelength
-    ub = sf[scan_no].UB
-
+    temp.append(sf[scan_no].Tsam)
+    motors.append(sf[scan_no].getSIXCAngles())
+    wavelength.append(sf[scan_no].wavelength)
+    ub.append(sf[scan_no].UB)
     if output_images:
         spec_scan = sf[scan_no]
         fp = FileProcessor(spec=spec_scan)
@@ -61,26 +54,12 @@ for scan_no in scan_nos:
             file = (numpy_path+"scan_"+str(scan_no)+"_img_"+str(idx))
             fnames.append(file)
             np.save(file=file, arr=image_stack[idx])
-
-            curtemp = temp[idx]
-            curmotors = motors[idx].tolist()
-            curwavelength = wavelength
-            curub = ub.tolist()
-            # print("file: {0}, type: {1}".format(file, file.__class__))
-            # print("sample temperature: {0}, type: {1}".format(curtemp,
-            #                                                   curtemp.__class__))
-            # print("ub: {0}, type: {1}".format(curub, curub.__class__))
-            # print("motors: {0}, type: {1}".format(curmotors,
-            #                                       curmotors.__class__))
-            # print("wavelength: {0}, type: {1}".format(curwavelength,
-            #                                           curwavelength.__class__))
-            record(scan_id=scan_no, descriptor_name='hkl_scan', seq_no=idx,
-                   data={'img': file,
-                         'sample temperature': curtemp,
-                         'ub': curub,
-                         'motors': curmotors,
-                         'wavelength': curwavelength})
         img_files.append(fnames)
+
+np.save(file=broker_path+"motors", arr=motors)
+np.save(file=broker_path+"temp", arr=temp)
+np.save(file=broker_path+"wavelength", arr=wavelength)
+np.save(file=broker_path+"ub", arr=ub)
 
 print("wavelength: {0}".format(wavelength))
 for idx, (T) in enumerate(temp):
