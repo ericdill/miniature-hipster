652611d75803d5ef00a13775953cc30f5ca635f3
Eric Dill <edill@bnl.gov>
2014-10-01 09:46:37 -0400

    ENH: Added offset and conversion for y-axis

--- miniature-hipster/plotting/waterfall.py
+++ miniature-hipster/plotting/waterfall.py
@@ -17,7 +17,7 @@ from scipy.signal import savgol_filter
 from scipy.interpolate import UnivariateSpline
 # pandas
 import pandas as pd
-from pandas import DataFrame, Series
+from pandas import DataFrame, Series, Index
 from pprint import pprint
 ################################################################################
 
@@ -27,18 +27,20 @@ __author__ = 'edill'
 folder = 'C:\\DATA\\New folder\\Data_4_Eric\\SCAN_Xr0'
 folder = 'C:\\DATA\\New folder\\Data_4_Eric\\SCAL_alongX'
 
+# todo plot multiple waterfalls on the same figure
+
 #### PLOTTING OPTIONS ##########################################################
 x_label = '2-theta (degrees)'
-y_label = 'Frame number'
+y_label = 'Depth (um)'
 z_label = 'Intensity (arb.)'
-min_x = 42
-max_x = 60
-min_y = None
-max_y = None
+min_x = None
+max_x = None
+start_frame = None
+end_frame = None
 min_z = 0
 max_z = None
 alpha = 1
-smooth = True
+smooth = False
 smooth_window_length = 91
 smooth_poly_order = 7  # must be odd!
 space_between_frames = 1  # y-spacing for each line
@@ -47,6 +49,8 @@ space_between_frames = 1  # y-spacing for each line
 color = 'hot_r'
 cstart = 0.3
 cstop = 0.7
+frame_offset = 0
+frame_delta = 0.2
 
 print_color_options = True
 if print_color_options:
@@ -56,17 +60,19 @@ if print_color_options:
 # init some parameters
 norm = cm.colors.Normalize(vmin=0, vmax=1)
 files = os.listdir(folder)
+# do in-place sorting of files
+files.sort()
 pprint(files)
 data = DataFrame()
 smoothed = DataFrame()
 
 # init the defaults
-if min_y is None:
-    min_y = 0
-if max_y is None:
-    max_y = len(files)
+if start_frame is None:
+    start_frame = 0
+if end_frame is None:
+    end_frame = len(files)
 
-for fname in files[min_y:max_y]:
+for fname in files[start_frame:end_frame]:
     with open(folder + os.sep + fname, 'r') as f:
         # data.append(np.asarray([line.split() for line in f.next()]).T)
         x, y = np.asarray([[float(val) for val in line.split()] for line in f]).T
@@ -93,15 +99,16 @@ if smooth:
         len(y)
         smoothed[d] = Series(y, indices)
 
-    smoothed.ix[0] = 0
-    smoothed.ix[-1] = 0
     to_plot = smoothed
 
 else:
-    data.ix[0] = 0
-    data.ix[-1] = 0
+    # data.ix[0] = 0
+    # data.ix[-1] = 0
     to_plot = data
-
+to_plot.ix[0] = 0
+to_plot.ix[-1] = 0
+# to_plot.index.insert(0, 0)
+# to_plot.index.insert(len(data.index), 0)
 # set the min and max values for z after smoothing
 if min_z is None:
     min_z = np.min([np.min(data[col_name]) for col_name in data])
@@ -136,15 +143,17 @@ fig = plt.figure()
 ax3 = Axes3D(fig)
 
 # set the offset of each polygon
-nz = len(data) * space_between_frames
-zs = np.arange(0, nz, space_between_frames)
+start = frame_offset
+end = start + frame_delta * len(data.columns)
+zs = np.arange(start, end, frame_delta)
+
 # add the polygons to the
 ax3.add_collection3d(poly, zs=zs, zdir='y')
 ax3.set_xlabel(x_label)
 ax3.set_ylabel(y_label)
 ax3.set_zlabel(z_label)
 ax3.set_xlim3d(min_x, max_x)
-ax3.set_ylim3d(min_y, max_y)
+ax3.set_ylim3d(start, end)
 ax3.set_zlim3d(min_z, max_z)
 
 # show the plot
